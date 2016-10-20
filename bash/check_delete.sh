fb4ba68567321a036e772d529d6a64d70210828f
Justin Collins <justin@presidentbeef.com>
2016-10-20 03:31:51 -0700

    File/line in grep

--- bash/check_delete.sh
+++ bash/check_delete.sh
@@ -2,4 +2,4 @@ git checkout $@ --quiet
 
 files=$(git diff --name-status master HEAD | grep -E "^(A|M)" | cut -f 2)
  
-grep "delete_survey([^,)]\+)" $files
+grep -H -n "delete_survey([^,)]\+)" $files
